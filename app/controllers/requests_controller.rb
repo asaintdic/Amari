a68608fa2bc07c27b58f34c24a95de665f9f458b
Alain <alains214@gmail.com>
2020-07-26 23:54:05 -0400

    Views and COntrollers all working

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,48 +1,53 @@
 class RequestsController < ApplicationController
     
-# get "/request" do
-# @requests = Request.all
-# erb :'request/index'
-# end 
+get "/request" do
+@req = Request.all
+erb :'request/index'
+end 
 
-# get "/request/show" do 
-#  erb :'/request/show'
-# end
+get "/request/show" do 
+ erb :'/request/show'
+end
  
-# get '/request/new' do
-# erb :'/request/new'
-# end
+get '/request/new' do
+erb :'/request/new'
+end
 
-# post '/request/new' do 
-#  @request = current_user.requests.build(gender: params[:gender], age: params[:age], clothing_size: params[:clothing_size], shoe_size: params[:shoe_size], add_info: params[:add_info], user_id: params[:user_id])
-#  if @request.save
-#   redirect "/request/show"
-#  else
-#   redirect "/request/new"
-#  end
-# end 
+post '/request/new' do 
+ @req = current_user.requests.build(gender: params[:gender], age: params[:age], clothing_size: params[:clothing_size], shoe_size: params[:shoe_size], add_info: params[:add_info], user_id: params[:user_id])
+ if @req.save
+  redirect "/request/show"
+ else
+  redirect "/request/new"
+ end
+end 
 
-# get '/request/:id' do
-#  @request = Request.find_by(params[:id])
-#  erb :'/request/show'
-# end
+get '/request/:id' do
+ @req = Request.find_by(params[:id])
+ erb :'/request/show'
+end
 
-# get '/request/:id/edit' do
-#  @request = Request.find_by(params[:id])
-#  erb :'/request/edit'
-# end
+get '/request/:id/edit' do
+ @req = Request.find_by_id(params[:id])
+ erb :'/request/edit'
+end
 
-# patch '/request/:id' do
-#  @request = Request.find_by(params[:id])
-#  @request.update(params[:request])
-#  redirect to "/request/#{ @request.id }"
-# end
+patch '/request/:id' do
+ @req = Request.find_by_id(params[:id])
+ @req.id = params[:id]
+ @req.gender = params[:gender]
+ @req.age = params[:age]
+ @req.clothing_size = params[:clothing_size]
+ @req.shoe_size = params[:shoe_size] 
+ @req.save
+ redirect to "/request/show"
+end
 
-# delete "/request/:id/delete" do
-# @request = Request.find_by(params[:id]) 
-# @request.delete  
-# # Request.destroy(params[:id])
-# redirect to "/request/show"
-# end
+get "/request/:id/delete" do
+@req = Request.find_by(params[:id]) 
+@req.delete  
+# Request.destroy(params[:id])
+redirect to "/request/show"
+end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,53 +51,53 @@ class UsersController < ApplicationController
     erb :"users/donate"
   end
  
-  get "/request" do
-    @requests = Request.all
-    erb :'request/index'
- end 
+#   get "/request" do
+#     @requests = Request.all
+#     erb :'request/index'
+#  end 
 
- get "/request/show" do 
-     erb :'/request/show'
- end
+#  get "/request/show" do 
+#      erb :'/request/show'
+#  end
      
- get '/request/new' do
-   erb :'/request/new'
- end
+#  get '/request/new' do
+#    erb :'/request/new'
+#  end
  
- post '/request/new' do 
-     @request = current_user.requests.build(gender: params[:gender], age: params[:age], clothing_size: params[:clothing_size], shoe_size: params[:shoe_size], add_info: params[:add_info], user_id: params[:user_id])
-    #  @current_user = current_user
+#  post '/request/new' do 
+#      @request = current_user.requests.build(gender: params[:gender], age: params[:age], clothing_size: params[:clothing_size], shoe_size: params[:shoe_size], add_info: params[:add_info], user_id: params[:user_id])
+#     #  @current_user = current_user
      
-     if @request.save
+#      if @request.save
       
-      redirect "/request/show"
-     else
+#       redirect "/request/show"
+#      else
      
-      redirect "/request/new"
-     end
-  end 
+#       redirect "/request/new"
+#      end
+#   end 
 
-get '/request/:id' do
-     @request = Request.find_by(params[:id])
-     erb :'/request/show'
- end
+# get '/request/:id' do
+#      @request = Request.find_by(params[:id])
+#      erb :'/request/show'
+#  end
  
- get '/request/:id/edit' do
-     @request = Request.find_by(params[:id])
-     erb :'/request/edit'
-    end
+#  get '/request/:id/edit' do
+#      @request = Request.find_by(params[:id])
+#      erb :'/request/edit'
+#     end
  
- patch '/request/:id' do
-     @request = Request.find_by(params[:id])
-     @request.update(params[:request])
-     redirect to "/request/#{ @request.id }"
- end
+#  patch '/request/:id' do
+#      @request = Request.find_by(params[:id])
+#      @request.update(params[:request])
+#      redirect to "/request/#{ @request.id }"
+#  end
  
- get "/request/:id/delete" do
-  @request = Request.find_by(params[:id]) 
-  @request.delete  
-  # Request.destroy(params[:id])
-  redirect to "/request/show"
- end
+#  get "/request/:id/delete" do
+#   @request = Request.find_by(params[:id]) 
+#   @request.delete  
+#   # Request.destroy(params[:id])
+#   redirect to "/request/show"
+#  end
   
 end
